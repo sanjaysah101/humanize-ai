2e791449e004ec5d64ec4b2105e8b44ec07c6366
parthsali <parthsali04@gmail.com>
2025-10-10 12:45:12 +0530

    chore(ui): Update ShadCN components and fix linting errors
    - Upgraded all ShadCN UI components to their latest versions using [?25l[2K[1G[36m?[39m [1mWhich components would you like to add?[22m [90m›[39m [90mSpace to select. A to toggle all. Enter to submit.[39m
    ◯   [36m[4maccordion[39m[24m[90m[39m
    ◯   alert[90m[39m
    ◯   alert-dialog[90m[39m
    ◯   aspect-ratio[90m[39m
    ◯   avatar[90m[39m
    ◯   badge[90m[39m
    ◯   breadcrumb[90m[39m
    ◯   button[90m[39m
    ◯   button-group[90m[39m
    ◯ ↓ calendar[90m[39m[2K[1G[32m✔[39m [1mWhich components would you like to add?[22m [90m›[39m
    [?25hNo components selected. Exiting.
    
    - Ran  to resolve formatting inconsistencies introduced by the component updates
    
    - Manually refactored function declarations in  and  to arrow functions to comply with the  ESLint rule

--- src/components/ui/progress.tsx
+++ src/components/ui/progress.tsx
@@ -8,10 +8,7 @@ import { cn } from "@/lib/utils";
 
 const Progress = React.forwardRef<
   React.ElementRef<typeof ProgressPrimitive.Root>,
-  React.ComponentPropsWithoutRef<typeof ProgressPrimitive.Root> & {
-    className?: string;
-    value?: number;
-  }
+  React.ComponentPropsWithoutRef<typeof ProgressPrimitive.Root>
 >(({ className, value, ...props }, ref) => (
   <ProgressPrimitive.Root
     ref={ref}
